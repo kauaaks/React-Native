a2c8c95b45cdcae1f881041f05a9cba78810d537
Kauã Aksenow <kaua.mota2@etec.sp.gov.br>
2025-03-17 20:59:04 -0300

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray', 
+    borderWidth: 1,
+    marginBotton: 12,
+    paddingHorizontal: 8,
+  },
+});
